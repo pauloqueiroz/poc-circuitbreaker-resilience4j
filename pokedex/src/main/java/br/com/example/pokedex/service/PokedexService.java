21c68dbac5d7e23fb44f3b35f22b29edeaa98faa
Paulo Sérgio <pauloqueiroz16@gmail.com>
2025-06-30 22:48:29 -0300

    Adds log4j2 integration

--- pokedex/src/main/java/br/com/example/pokedex/service/PokedexService.java
+++ pokedex/src/main/java/br/com/example/pokedex/service/PokedexService.java
@@ -1,15 +1,18 @@
 package br.com.example.pokedex.service;
 
+import br.com.example.pokedex.kanto.client.KantoClient;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.mvc.ProxyExchange;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import br.com.example.pokedex.kanto.client.KantoClient;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-
 @Service	
 public class PokedexService {
+
+	private static final Logger LOG = LogManager.getLogger("Log4JCore");
 
 	KantoClient kantoClient;
 	
@@ -20,6 +23,9 @@ public class PokedexService {
 
 	@CircuitBreaker(name="kantoService")
 	public ResponseEntity<?> getRegionData(String region){
+		LOG.info("Regiao buscada: "+region);
+		LOG.warn("Warning - Regiao buscada: "+region);
+		LOG.error("Error - Regiao buscada: "+region);
 		var kantoData = kantoClient.retrieveKantoData();
 		return ResponseEntity.ok(kantoData.toString());
 	}
